ec5d6df0b7a4637c18992987cc3fed49335f8c21
Nicha <NichaRoj@gmail.com>
2020-05-22 21:48:30 +0700

    Create lesson1

---
+++ lesson1/src/index.ts
@@ -0,0 +1,8 @@
+// Variables (Global & Local)
+// Data types
+// Keywords: for, while (or switch case), function (or method), interface, if/else
+// Special symbols: > >= < <= == === != !== && || += =+ -= =-
+// Special types: array and its methods, lambda
+// import export
+
+console.log("Hello world!");
