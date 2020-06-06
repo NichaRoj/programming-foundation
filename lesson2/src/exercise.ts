3cd15ceac6f9e17ce758aa2b6849bf60d324172f
Nicha <NichaRoj@gmail.com>
2020-06-06 10:51:39 +0700

    Create lesson2

---
+++ lesson2/src/exercise.ts
@@ -0,0 +1,12 @@
+/**
+ * Exercise!
+ *
+ * Let's try creating some interfaces and classes!
+ * Imagine you are the owner of a pizzaria. You would need to create pizzas!
+ * First, create a template for pizzas on the menu. Each pizza should have a name and a list (or array) of toppings!
+ *
+ * Then, because we are in the future, we can just create a pizza maker!
+ * Try creating a class of PizzaMaker which has a list of pizzas on the menu (which we will call menu for short),
+ * and a function to create a pizza! This `create` function should have a name as an input
+ * and return an object with the name and its toppings as an output.
+ */
---
+++ lesson2/src/index.ts
@@ -0,0 +1,103 @@
+/** Object-Oriented Programming
+ *
+ * Goal: Reuse components and enforce type checks
+ * Characteristics:
+ *    - Abstraction: The process of picking out (abstracting) common features of objects and procedures.
+ *    - Encapsulation: The process of combining elements to create a new entity. A procedure is a type of encapsulation because it combines a series of computer instructions.
+ *    - Inheritance: a feature that represents the "is a" relationship between different classes.
+ *    - Polymorphism: A programming language's ability to process objects differently depending on their data type or class.
+ *
+ * Topics:
+ *    - Interfaces
+ *    - Classes
+ *    - Superclass/Subclass
+ */
+
+// Let's start with interface!
+// If you recall, Typescript has basic data types which can be explicitly assigned to variables.
+// However, what if we want to give a variable a more complex type?
+// For example, a bank account would have at least a name and the total number of money in that account.
+// Since the type is more complex than the types Typescript has, the type would fall under `object`
+// `object` is an umbrella type for any data types that we create ourselves.
+
+let bankAccountA = {
+  name: "Meen",
+  total: 4671.23,
+};
+
+// This variable will have the type `object` which has `name` and `total` as its properties.
+// But what if we want to define this type to variables or inputs of a function?
+
+let bankAccountB: {
+  name: string;
+  total: number;
+};
+
+let someFunc = (account: { name: string; total: number }) => account;
+
+// But we don't want to repeat it again and again, so we will create an interface for it!
+// And since function is a special data type, we can give a property with a function too!
+interface BankAccountInt {
+  name: string;
+  total: number;
+  deposit: (amount: number) => void;
+  withdraw: (amount: number) => void;
+}
+
+// Now we can just assign the interface anywhere we want
+let bankAccountC: BankAccountInt;
+
+// However, deposit and withdraw should work the same way for all bank accounts
+// But we still need to define deposit and withdraw every time we create a BankAccount
+bankAccountC = {
+  name: "Nicha",
+  total: 350.42,
+  deposit: (amount: number) => {
+    this.total += amount;
+  }, // This means this.total = this.total + amount
+  withdraw: (amount: number) => {
+    this.total -= amount;
+  }, // This means this.total = this.total - amount
+};
+
+// It will still work though
+bankAccountC.deposit(50);
+console.log(`bankAccountC Total: ${bankAccountC.total}`);
+
+// If `interface` is just a blueprint, then a `class` is a robot that will start working whenever we want
+class BankAccount {
+  constructor(public name: string, public total: number) {} // name and total become BankAccount's properties
+
+  public deposit(amount: number) {
+    this.total += amount;
+  }
+
+  public withdraw(amount: number) {
+    this.total -= amount;
+  }
+}
+
+// Now we can easily create a bank account
+let bankAccountD = new BankAccount("Emma", 0);
+bankAccountD.deposit(500);
+console.log(`bankAccountD Total: ${bankAccountD.total}`);
+
+// This new account is called an instance of a BankAccount.
+// Now, let's take note of some keywords.
+// `this` is usually used within a class or an object to point to its properties instead of any variables outside
+// `public` defines the scope where the properties or functions can be called.
+// If a property or a function is `private` then it cannot be called outside of that instance.
+// There is also a keyword `static` which means that property or function can be used without creating an instance.
+
+class Message {
+  // constructor is not needed if there is no non-static properties.
+  static INFO = 0;
+  static WARNING = 1;
+  static ERROR = 2;
+  static print = (msg: string) => {
+    console.log(msg);
+  };
+}
+
+// Those properties can be called by refering to the class
+Message.print(`${Message.INFO}`);
