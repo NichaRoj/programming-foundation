f2addeaa6d8e2afa905b9c4101e1e49c0ae3261c
Meen1995 <Lynxderek@gmail.com>
2020-06-06 14:39:24 +0700

    complete exercise in lesson2

--- lesson1/src/index.ts
+++ lesson1/src/index.ts
@@ -1,3 +1,13 @@
 // To run this code, navigate to `programming-foundation/lesson1` in terminal
 // and type `yarn start`!
 console.log("Hello world!");
+
+let arr1 = ["Rapunzel", "Merida", "Cinderella"];
+function printArray(arr){
+    for (let name of arr){
+        console.log(`I am princess ${name}`)
+}
+}
+printArray(arr1)
+let arr2 = ["Ariel", "Mulan", "Aurora", "Pocahontas"];
+printArray(arr2)
--- lesson2/src/exercise.ts
+++ lesson2/src/exercise.ts
@@ -10,3 +10,33 @@
  * and a function to create a pizza! This `create` function should have a name as an input
  * and return an object with the name and its toppings as an output.
  */
+interface Pizza{
+    name: string;
+    toppings: string[]
+}
+// Pizza below is to tell that the datatype is Pizza not 'any'
+const hawaiian: Pizza = {
+    name: "hawaiian", 
+    toppings: ["pineapple", "ham", "cheese"]
+}
+console.log(`the toppings in ${hawaiian.name} pizza are:`)
+for (let topping of hawaiian.toppings){
+    console.log(topping)
+}
+
+
+
+class PizzaMaker{
+    constructor(public menu: Pizza[] = []){}
+    public makePizza(name: string){
+        let foundPizza = this.menu.find((value) => value.name === name)
+        if (foundPizza){
+            console.log(`here's your ${foundPizza.name} pizza: ${foundPizza.toppings.join(", ")}`)
+        }else{
+            console.log(`sorry we cannot serve ${name} here`)
+        }
+    }
+}
+const meenicha = new PizzaMaker([hawaiian])
+meenicha.makePizza(`hawaiian`)
+meenicha.makePizza(`fourcheese`)
