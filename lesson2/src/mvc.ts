6336b595d5784f470085b94b953509e1e81e6147
Nicha <NichaRoj@gmail.com>
2020-06-20 14:38:18 +0700

    Add mvc to lesson2

---
+++ lesson2/src/mvc.ts
@@ -0,0 +1,16 @@
+// Next, let's learn about software architecture!
+// But let's not try to define what a SA is but look at what it is used for.
+// Try to go on a website and then try to define each component what it is used for.
+// You will find that...damn there's a lot! Seriously, a lot!
+// And when there are so many of them, then how do you keep your code base clean?
+// In addition, how do you make sure that any changes in the future would not destroy what's already been done?
+// Software Architecture helps with that! It separates a project into more manageable components.
+// And there are many practices for that. And people still keep coming up with new ones right now.
+
+// Today we are going to learn MVP. That's short for Model-View-Presenter
+// If you are not instructed by me then you should google!
+// And read the comparison of MVP with MVC and MVVM as well!
+// But most importantly, don't forget to look at the examples! It really helps.
+
+// However, the best way to learn is to try our hands on it!
+// So let's make a simple terminal-based ATM where we can register a new account, and also deposit/withdraw from the account as well.
